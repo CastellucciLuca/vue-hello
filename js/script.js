6c6c5ef096f9fceb77d28aa7ad0bfd475cac20f6
CastellucciLuca <castellucciluca98@gmail.com>
2022-12-12 15:05:49 +0100

    add Vue img

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ createApp({
 	data() {
 		return{
 			title: "Hello Vue World!",
+        img: "https://swimjiy.github.io/static/27f1c5517910ba9a7cfc6a216c4b1f78/38a7d/vue.jpg"
 		}
 	}
 }).mount ('#app');
